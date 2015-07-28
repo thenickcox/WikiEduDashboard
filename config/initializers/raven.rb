1c614aea254eb00f81604e291c1409578f227161
Nick Cox <nick@nickcox.me>
2015-07-28 11:54:16 -0700

    Prepend article language to article title (Closes #199)

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,6 +1,8 @@
 require 'raven'
 
-Raven.configure do |config|
-  config.dsn = Figaro.env.sentry_dsn
-  config.silence_ready = true
+unless Rails.env.test?
+  Raven.configure do |config|
+    config.dsn = Figaro.env.sentry_dsn
+    config.silence_ready = true
+  end
 end
